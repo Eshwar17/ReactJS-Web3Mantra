2cfe7f442c8b3bc39a17dfd8bb7e81a2bf4960b0
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-12 12:07:36 +0530

    Practiced multiple imports and exports

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -1,14 +1,11 @@
-
 import './App.css';
-
+import { Header, Footer } from './Header';
 function App() {
   const msg = <h1>I am Eshwar</h1>;
   return (
     <div className="App">
-      <header className="App-header">
-      {msg}
-      <h3>We can use JavaScript directly using curly braces <span style={{color: 'blue'}}>{34 + 45}</span></h3>
-      </header>
+      <Header />
+      <Footer />
     </div>
   );
 }
---
+++ react_course/src/Header.jsx
@@ -0,0 +1,2 @@
+export const Header = () => <h1>Header</h1>
+export const Footer = () => <h2>Footer</h2>
