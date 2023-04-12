819514ab1575aafeec6c91469d28064c6f6eb8ad
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-12 15:07:43 +0530

    Practiced prop-types

--- react_course/src/components/Student.js
+++ react_course/src/components/Student.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import propTypes from 'prop-types'
 
 const Student = (props) => {
   return (
@@ -6,6 +7,11 @@ const Student = (props) => {
       <h1>Hi {props.name}! You are {props.age} years old!!</h1>
     </div>
   )
+}
+
+Student.propTypes = {
+    name: propTypes.string,
+    age: propTypes.number,
 }
 
 export default Student
