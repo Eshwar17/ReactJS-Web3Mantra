932644140e513c0ede1392677596861e52ec942b
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-14 22:16:53 +0530

    Updated Home.js and rendered it in App.js

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -1,22 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
-
+import Home from './components/Home';
 
 function App() {
 
-  const [state, setState] = useState(10);
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    async function getData() {
-      const get = await fetch(`https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`);
-      const res = await get.json();
-      setData(res);
-      console.log(res);
-    }
-    getData();
-    document.title = `(${state}) Employees Online`;
-  }, [state]) 
+  
 
 // [] -> useEffect will be called only once i.e, at the beginning of the rendering
 //We can add dependencies by separating them with commas
@@ -26,19 +15,8 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       
-      {
-        data.map((element, index) => {
-          return (
-            <div className='data' key={index}>
-              <h5>{element.firstName}</h5>
-              <h5>{element.lastName}</h5>
-              <h5>{element.email}</h5>
-            </div>
-          )
-        })
-      }
-      <button onClick={() => setState(state+10)}>Load More {state}</button>
     </div>
   );
 }
--- react_course/src/components/Home.js
+++ react_course/src/components/Home.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Home = () => {
+    const [state, setState] = useState(10);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      const get = await fetch(`https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`);
+      const res = await get.json();
+      setData(res);
+      console.log(res);
+    }
+    getData();
+    document.title = `(${state}) Employees Online`;
+  }, [state]) 
   return (
     <div>
-      
+      {
+        data.map((element, index) => {
+          return (
+            <div className='data' key={index}>
+              <h5>{element.firstName}</h5>
+              <h5>{element.lastName}</h5>
+              <h5>{element.email}</h5>
+            </div>
+          )
+        })
+      }
+      <button onClick={() => setState(state+10)}>Load More {state}</button>
     </div>
   )
 }
