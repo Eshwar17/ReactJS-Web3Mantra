340b02a60d5cb67e46cddf42aedfa230ee4e5599
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-14 11:51:37 +0530

    Added dependency to useEffect hook

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -4,16 +4,22 @@ import Header from './components/Header';
 
 
 function App() {
+
+  const [state, setState] = useState(0);
+
   useEffect(() => {
     console.log("UseEffect called")
-  })
+  }, [state]) 
+
+// [] -> useEffect will be called only once i.e, at the beginning of the rendering
+//We can add dependencies by separating them with commas
 
   console.log("Fn body called")
 
   return (
     <div className="App">
       <Header />
-      {console.log("INside JSX")}
+      <button onClick={() => setState(state+1)}>Click Me {state}</button>
     </div>
   );
 }
