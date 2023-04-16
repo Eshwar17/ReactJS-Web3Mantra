a397facef47f87bc96279c44bb855c414eb316b3
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-16 10:46:03 +0530

    Created Comp1, Comp2, Comp3 components for learning context API

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -1,79 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Home from "./components/Home";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  NavLink,
-} from "react-router-dom";
-import Contact from "./components/Contact";
-import About from "./components/About";
-import Error from "./components/Error";
-import Insta from "./components/Insta";
-import Mail from "./components/Mail";
-import UserDetails from "./components/UserDetails";
+import Comp1 from "./components/Comp1";
 
 function App() {
-  // [] -> useEffect will be called only once i.e, at the beginning of the rendering
-  //We can add dependencies by separating them with commas
-
-  // console.log("Fn body called")
-  const navLinkStyles = ({ isActive }) => {
-    return {
-      color: isActive ? "red" : "black",
-      fontSize: "25px",
-      textDecoration: isActive ? "underline" : "none",
-    };
-  };
+  const [data, setData] = useState("Eshwar Context API");
 
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <nav>
-          <ul
-            style={{
-              backgroundColor: "white",
-              display: "flex",
-              justifyContent: "space-between",
-              padding: "0 2rem",
-            }}
-          >
-            <li>
-              <NavLink style={navLinkStyles} to="/">
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink style={navLinkStyles} to="/about">
-                About
-              </NavLink>
-            </li>
-            <li>
-              <NavLink style={navLinkStyles} to="/contact">
-                Contact
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/:id" element={<UserDetails />} />
-
-          <Route path="/about" element={<About />} />
-
-          <Route path="/contact" element={<Contact />}>
-            <Route index element={<Insta />} />
-            <Route path="insta" element={<Insta />} />
-            <Route path="mail" element={<Mail />} />
-          </Route>
-
-          <Route path="*" element={<Error />}></Route>
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Header />
+      <Comp1 />
+    </>
   );
 }
 
--- react_course/src/components/Home.js
+++ react_course/src/components/Home.js
@@ -1,35 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 
 const Home = () => {
-  const [state, setState] = useState(10);
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    async function getData() {
-      const get = await fetch(
-        `https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`
-      );
-      const res = await get.json();
-      setData(res);
-      console.log(res);
-    }
-    getData();
-    document.title = `(${state}) Employees Online`;
-  }, [state]);
+  
   return (
     <div>
-      {data.map((element, index) => {
-        return (
-          <Link to={`/${element.id}`}>
-            <div className="data" key={index}>
-              <h5>{element.firstName}</h5>
-              <h5>{element.lastName}</h5>
-              <h5>{element.email}</h5>
-            </div>
-          </Link>
-        );
-      })}
-      <button onClick={() => setState(state + 10)}>Load More {state}</button>
+      Home
     </div>
   );
 };
